0a8375399015a8b38a33bd475a3af45a3cccabe9
Gimenez <32960304+FR12Gimenez@users.noreply.github.com>
2023-08-22 21:43:14 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+
+def send_message(webhook_url, message):
+    data = {
+        "content": message
+    }
+    response = requests.post(webhook_url, json=data)
+
+    if response.status_code == 204:
+        print("Message sent")
+    else:
+        print("Did not send message or banned with the error code: {status_code}")
+
+def main():
+    webhook_url = input("Enter the webhook URL: ")
+    
+    while True:
+        user_input = input("Enter a message: ")
+        
+        if user_input.lower() == "exit":
+            break
+        
+        send_message(webhook_url, user_input)
+
+if __name__ == "__main__":
+    main()
